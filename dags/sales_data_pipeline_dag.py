8d1e456c9a9b37f3b840df89a2a4f545473503a7
ericson-matheus <ericson_matheus.250@hotmail.com>
2025-05-09 23:49:30 -0300

    sales data pipeline (feat): create data and extract sales and employees

---
+++ dags/sales_data_pipeline_dag.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
+
+from utils.extractors import (
+    extract_sales_from_postgres,
+    extract_employees_from_api,
+)
+
+sales_engine = PostgresHook(postgres_conn_id="db_sales").get_sqlalchemy_engine()
+
+default_args = {
+    "owner": "ericson",
+    "depends_on_past": False,
+    "email": ["ericson.matheus.2016@gmail.com"],
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=3),
+    "execution_timeout": timedelta(minutes=10),
+}
+
+with DAG(
+    dag_id="sales_data_pipeline_dag",
+    default_args=default_args,
+    description="Pipeline de dados para análise de vendas",
+    schedule_interval="0 5 * * *",
+    start_date=datetime(2025, 5, 9),
+    catchup=False,
+    tags=["sales", "data-engineering"],
+    max_active_runs=1, # Ensures only one instance of this DAG runs at any given time
+) as dag:
+
+    extract_sales_task = PythonOperator(
+        task_id="extract_sales_data",
+        python_callable=extract_sales_from_postgres,
+        op_kwargs={
+            "engine": sales_engine,
+            "execution_date": "{{ ds }}",
+        },
+    )
+
+    extract_employees_task = PythonOperator(
+        task_id="extract_employees_data",
+        python_callable=extract_employees_from_api,
+        op_kwargs={
+            "execution_date": "{{ ds }}",
+        },
+    )
+
+    (
+        [extract_sales_task, extract_employees_task]
+    )
