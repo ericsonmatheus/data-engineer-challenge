eec7eb2c0de807c75d7d5bd873d45654bdaaa68e
ericson-matheus <ericson_matheus.250@hotmail.com>
2025-05-09 23:55:07 -0300

    config (feat): add config to access folders, api and db

---
+++ dags/utils/config.py
@@ -0,0 +1,9 @@
+import os
+
+# Paths for files and directories used in the pipeline
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DATA_PATH = os.path.join(BASE_DIR, 'data')
+
+EMPLOYEES_API_CONFIG = {
+    'base_url': os.environ.get('EMPLOYEES_API_URL'),
+}
