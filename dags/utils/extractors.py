92415d2a894c7e797245309c12fa875b7f048b1d
ericson-matheus <ericson_matheus.250@hotmail.com>
2025-05-10 14:13:26 -0300

    extractors (feat): add function to get and save categories file

--- dags/utils/extractors.py
+++ dags/utils/extractors.py
@@ -1,16 +1,13 @@
-import os
 import logging
+import os
+
 import pandas as pd
 import requests
-
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-
-from utils.config import (
-    DATA_PATH,
-    EMPLOYEES_API_CONFIG,
-)
+from utils.config import CATEGORIES_URL_PARQUET, DATA_PATH, EMPLOYEES_API_CONFIG
 
 logger = logging.getLogger(__name__)
+
 
 def extract_sales_from_postgres(engine: PostgresHook, execution_date: str) -> None:
     """
@@ -34,7 +31,9 @@ def extract_sales_from_postgres(engine: PostgresHook, execution_date: str) -> No
         output_file_path = f"{output_dir}/sales_data.parquet"
         sales_df.to_parquet(output_file_path, index=False)
 
-        logger.info(f"Sales data successfully extracted: {len(sales_df)} records saved to {output_file_path}")
+        logger.info(
+            f"Sales data successfully extracted: {len(sales_df)} records saved to {output_file_path}"
+        )
 
     except Exception as e:
         logger.error(f"Error while extracting sales data from PostgreSQL: {str(e)}")
@@ -49,9 +48,11 @@ def extract_employees_from_api(execution_date: str) -> None:
         execution_date (str): Execution date in the format 'YYYY-MM-DD'.
     """
     try:
-        logger.info(f"Initiating employee data extraction from the API for the date: {execution_date}.")
+        logger.info(
+            f"Initiating employee data extraction from the API for the date: {execution_date}."
+        )
 
-        base_url = EMPLOYEES_API_CONFIG['base_url']
+        base_url = EMPLOYEES_API_CONFIG["base_url"]
 
         all_employees = []
         has_more = True
@@ -63,15 +64,19 @@ def extract_employees_from_api(execution_date: str) -> None:
 
             if response.status_code == 200:
                 data = response.text.strip()
-                
+
                 if data != "The argument is not correct":
                     all_employees.append({"id": employee_id, "name": data})
                     employee_id += 1
                 else:
                     has_more = False
-                    logger.warning(f"No more data found after employee ID {employee_id-1}.")
+                    logger.warning(
+                        f"No more data found after employee ID {employee_id-1}."
+                    )
             else:
-                logger.error(f"Failed to retrieve data for employee ID {employee_id}. HTTP Status: {response.status_code}")
+                logger.error(
+                    f"Failed to retrieve data for employee ID {employee_id}. HTTP Status: {response.status_code}"
+                )
                 break
 
         if not all_employees:
@@ -87,8 +92,43 @@ def extract_employees_from_api(execution_date: str) -> None:
         output_file_path = f"{output_dir}/employees_data.parquet"
         employees_df.to_parquet(output_file_path, index=False)
 
-        logger.info(f"Successfully extracted {len(employees_df)} employee records. Data saved to: {output_file_path}")
+        logger.info(
+            f"Successfully extracted {len(employees_df)} employee records. Data saved to: {output_file_path}"
+        )
 
     except Exception as e:
-        logger.error(f"An error occurred while extracting employee data from the API: {str(e)}")
+        logger.error(
+            f"An error occurred while extracting employee data from the API: {str(e)}"
+        )
+        raise
+
+
+def extract_categories_from_parquet(execution_date: str) -> None:
+    """
+    Extracts category data from a Parquet file hosted on Google Cloud Storage
+    and saves it locally as a new Parquet file.
+
+    Args:
+        execution_date (str): Execution date in the format 'YYYY-MM-DD'.
+    """
+    try:
+        logger.info(
+            f"Starting category data extraction from GCS: {CATEGORIES_URL_PARQUET}"
+        )
+
+        categories_df = pd.read_parquet(CATEGORIES_URL_PARQUET, engine="pyarrow")
+
+        # Create the directory if it doesn't exist
+        output_dir = f"{DATA_PATH}/raw_data/categories/{execution_date}"
+        os.makedirs(output_dir, exist_ok=True)
+
+        output_file_path = f"{output_dir}/categories_data.parquet"
+        categories_df.to_parquet(output_file_path, index=False)
+
+        logger.info(
+            f"Successfully extracted {len(categories_df)} categories records. Data saved to: {output_file_path}"
+        )
+
+    except Exception as e:
+        logger.error(f"Error while extracting category data from GCS: {str(e)}")
         raise
