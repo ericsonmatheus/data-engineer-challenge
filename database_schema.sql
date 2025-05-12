c0485f3d04a447442e26cbdca1ebf8a40ac0673d
ericson-matheus <ericson_matheus.250@hotmail.com>
2025-05-12 08:54:17 -0300

    database schema (feat): add trigger and columns of _created and _updated

--- database_schema.sql
+++ database_schema.sql
@@ -7,20 +7,51 @@ CREATE DATABASE "sales-db"
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
--- Agora conecte ao banco de dados recém-criado
--- Este comando funciona no psql, mas se você estiver usando uma ferramenta gráfica (como DBeaver ou pgAdmin),
--- apenas abra uma nova conexão com o banco `sales_db`
-\c sales_db
+-- Criação de Trigger para atualizar a coluna _updated
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW._updated = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
 
 -- Tabela de categorias
 CREATE TABLE IF NOT EXISTS public.categories
 (
     id bigint NOT NULL,
-    category_name character varying(30)[] NOT NULL,
+    category_name character varying NOT NULL,
+    _created timestamp without time zone NOT NULL DEFAULT now(),
+    _updated timestamp without time zone NOT NULL DEFAULT now() ON UPDATE now(),
     PRIMARY KEY (id)
 )
 TABLESPACE pg_default;
+
+CREATE TRIGGER update_categories_modtime
+BEFORE UPDATE ON public.categories
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
+
 ALTER TABLE IF EXISTS public.categories
+    OWNER to airflow;
+
+-- Tabela de funcionários
+CREATE TABLE IF NOT EXISTS public.employees
+(
+    id integer NOT NULL,
+    name character varying NOT NULL,
+    _created timestamp without time zone NOT NULL DEFAULT now(),
+    _updated timestamp without time zone NOT NULL DEFAULT now() ON UPDATE now(),
+    PRIMARY KEY (id)
+)
+TABLESPACE pg_default;
+
+CREATE TRIGGER update_employees_modtime
+BEFORE UPDATE ON public.employees
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
+
+ALTER TABLE IF EXISTS public.employees
     OWNER to airflow;
 
 -- Tabela de vendas
@@ -31,28 +62,26 @@ CREATE TABLE IF NOT EXISTS public.sales
     category_id integer NOT NULL,
     sale_date date NOT NULL,
     sale_value numeric(20, 2) NOT NULL,
+    _created timestamp without time zone NOT NULL DEFAULT now(),
+    _updated timestamp without time zone NOT NULL DEFAULT now(),
     PRIMARY KEY (id),
-    CONSTRAINT fk_sale_employee FOREIGN KEY (employee_id)
-        REFERENCES public.employee (id) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-        NOT VALID,
-    CONSTRAINT fk_sale_categories FOREIGN KEY (category_id)
+    CONSTRAINT fk_sales_categories FOREIGN KEY (category_id)
         REFERENCES public.categories (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
+        NOT VALID,
+    CONSTRAINT fk_sales_employees FOREIGN KEY (employee_id)
+        REFERENCES public.employees (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
         NOT VALID
-);
-ALTER TABLE IF EXISTS public.sales
-    OWNER to airflow;
-
--- Tabela de funcionários
-CREATE TABLE IF NOT EXISTS public.employee
-(
-    id integer NOT NULL,
-    name character varying(60) NOT NULL,
-    PRIMARY KEY (id)
 )
 TABLESPACE pg_default;
-ALTER TABLE IF EXISTS public.employee
+
+CREATE TRIGGER update_sales_modtime
+BEFORE UPDATE ON public.sales
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
+
+ALTER TABLE IF EXISTS public.sales
     OWNER to airflow;
